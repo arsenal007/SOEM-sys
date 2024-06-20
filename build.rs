fc4bfbac78a694fc270a3801bf3bab08caf3989d
arsenal007 <fc_arsenal@ukr.net>
2024-06-20 14:36:54 +0200

    link to libsoem.a

--- build.rs
+++ build.rs
@@ -1,28 +1,35 @@
 extern crate bindgen;
-extern crate cmake;
 
 use std::{env, format, path::PathBuf};
 
 fn main() {
-    let dst = cmake::Config::new("SOEM").build();
+    let soem_lib_path = "../SOEM_Project/build/";
+    let soem_include_path = "../SOEM/";
+    let soem_osal_include_path = "../SOEM/osal/"; 
+    let soem_osal_def_include_path = "../SOEM_Project/source/osal/";
+    let soem_oshw_include_path = "../SOEM_Project/source/oshw/";
 
-    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    println!("cargo:rustc-link-search=native={}", soem_lib_path);
     println!("cargo:rustc-link-lib=static=soem");
-    println!("cargo:include={}/include", dst.display());
+    println!("cargo:include={}", soem_include_path);
+    println!("cargo:osal_include={}", soem_osal_include_path);
 
     let bindings = bindgen::Builder::default()
-        .clang_arg(format!("-I{}/include", dst.display()))
-        .clang_arg(format!("-I{}/include/soem", dst.display()))
+        .clang_arg(format!("-I{}", soem_include_path))
+        .clang_arg(format!("-I{}/soem", soem_include_path))
+	.clang_arg(format!("-I{}", soem_osal_include_path))
+        .clang_arg(format!("-I{}", soem_osal_def_include_path))
+        .clang_arg(format!("-I{}", soem_oshw_include_path))
         .header("wrapper.h")
-        .whitelist_function("ec(x?)_(.*)")
-        .whitelist_type("ec_fmmu")
-        .whitelist_type("ec_group")
-        .whitelist_type("ec_slave")
-        .whitelist_type("ec_sm")
-        .whitelist_type("ec_state")
-        .whitelist_type("ecx_contextt")
-        .whitelist_type("ecx_portt")
-        .whitelist_type("ecx_redportt")
+        .allowlist_function("ec(x?)_(.*)")
+        .allowlist_type("ec_fmmu")
+        .allowlist_type("ec_group")
+        .allowlist_type("ec_slave")
+        .allowlist_type("ec_sm")
+        .allowlist_type("ec_state")
+        .allowlist_type("ecx_contextt")
+        .allowlist_type("ecx_portt")
+        .allowlist_type("ecx_redportt")
         .opaque_type("ec_PDOassignt")
         .opaque_type("ec_PDOdesct")
         .opaque_type("ec_SMcommtypet")
